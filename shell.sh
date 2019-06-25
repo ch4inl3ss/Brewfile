61018e7daaf03f6cb2cc3b9a6b5a675986946894
Felix Schumacher <ch4inl3ss@gmail.com>
2019-06-25 18:50:25 +0200

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-gem install colorls
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo gem install colorls
 echo "alias l='colorls -lA'" > .zshrc
 echo "alias ls='colorls -1A'" > .zshrc
 echo "alias ll='colorls -lA'" > .zshrc
